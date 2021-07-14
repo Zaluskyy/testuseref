ad2761f93e6347c15da204ce0746b46a47635531
Your-Full-Name <kryzal77@gmail.com>
2021-07-15 01:40:04 +0200

    jd kurwe

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   
-  const canvas = useRef(null)
+  // const canvas = useRef(null)
   
   class Magic{
     constructor(x, y){
@@ -11,20 +11,20 @@ function App() {
       this.y = y
     }
     draw(){
-      const c = canvas.current.getContext("2d");
-      c.arc(this.x, this.y, 50, 0, Math.PI*2, false)
-      c.fill()
+      // const c = canvas.current.getContext("2d");
+      // c.arc(this.x, this.y, 50, 0, Math.PI*2, false)
+      // c.fill()
     }
   }
   const magic = new Magic(200, 200)
 
-  useEffect(()=>{
-    animate()
-  })
+  // useEffect(()=>{
+  //   animate()
+  // })
 
-  const animate = ()=>{
-    magic.draw()
-  }
+  // const animate = ()=>{
+  //   magic.draw()
+  // }
 
 
 
@@ -32,7 +32,8 @@ function App() {
   return (
     
     <div className="App">
-      <canvas width={400} height={400} style={{background:"red"}} ref={canvas} ></canvas>
+      {/* <canvas width={400} height={400} style={{background:"red"}} ref={canvas} ></canvas> */}
+      jebać disa skurwysyna
     </div>
   );
 }
